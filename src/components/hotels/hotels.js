a57e4bac0cab525e375e223d1a18f7488d05c612
Lelonek123 <lelonek1234@gmail.com>
2022-09-02 11:08:13 +0200

    Initial commit, file structure ready

---
+++ src/components/hotels/hotels.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hotel from './hotel/hotel.js'
+
+function Hotels() {
+    return (
+        <div>
+            <h2>Hotels</h2>
+            <Hotel />
+            <Hotel />
+        </div>
+    )
+}
+
+export default Hotels;
